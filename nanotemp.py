e6bd7eebad455d5e23cf7951d6d046e9af3a688d
oph <ophthalmencephalon@gmail.com>
2021-12-07 01:13:25 +0000

    Amend argparse help strings

--- nanotemp.py
+++ nanotemp.py
@@ -9,7 +9,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Request temperature readings from remote Jetson Nano sensors.')
 parser.add_argument("server", help="Address of service host.")
 parser.add_argument("port", help="Listening port of service host.", type=int)
-parser.add_argument("sensor", help="Query a specific thermal sensor (GPU/CPU, optional).", nargs='?')#, choices=["GPU", "CPU", None])
+parser.add_argument("sensor", help="Query a specific thermal sensor (GPU/CPU, optional).", nargs='?')
 args = parser.parse_args()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
